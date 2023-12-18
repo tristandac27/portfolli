60ab3cfbbaf27aa24858047584840fd5401a51f2
tristan <tdacier@sicalde.fr>
2023-12-18 19:01:46 +0100

    ytes

---
+++ ContactModel.cs
@@ -0,0 +1,7 @@
+public class ContactModel
+{
+    public string? Nom { get; set; }
+    public string? Email { get; set; }
+    public string? Telephone { get; set; }
+    public string? Message { get; set; }
+}
