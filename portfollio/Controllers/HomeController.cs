2dff40b7e4a75f5c8b8bee6e7935c3eaeecc9ae8
Tristandac28 <tdacier@siclade.fr>
2023-12-18 16:21:56 +0100

    initial commit

---
+++ portfollio/Controllers/HomeController.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using portfollio.Models;
+using System.Diagnostics;
+
+namespace portfollio.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+        public IActionResult DownloadCV()
+        {
+            string filePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwoot", "CV.pdf");
+            byte[] fileBytes = System.IO.File.ReadAllBytes(filePath);
+            return File(fileBytes, "application/pdf", "CV.pdf");
+        }
+
+        public IActionResult CV()
+        {
+            return View();
+        }
+
+        public IActionResult Juridique()
+        {
+            return View();
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Techno()
+        {
+            return View();
+        }
+
+        public IActionResult about()
+        {
+            return View();
+        }
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+
+        public IActionResult Documents()
+        {
+            return View();
+        }
+      
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ portfollio/Views/Home/Techno.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace portfollio.Views.Home
+{
+    public class TechnoModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
